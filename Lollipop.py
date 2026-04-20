07b3a975311d69f13ad2461a8016080fcaf67b29
Nurul Anisa Najiha Mispan <anisanajiha171@gmail.com>
2026-04-20 11:18:57 -0600

    initial code for the lollipop plot

---
+++ Lollipop.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def lollipop_ts(earthquake):
+	plt.plot(new_time, new_mag, marker = 'o')
+	plt.xlabel("Date")
+	plt.ylabel("Earthquake Magnitude")
+	plt.title("Earthquake Magnitude Time Series Plot")
+	
