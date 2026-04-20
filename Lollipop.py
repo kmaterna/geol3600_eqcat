d185d70aaa67c3a8d01a7420ef66572d1f915d6f
Nurul Anisa Najiha Mispan <anisanajiha171@gmail.com>
2026-04-20 11:58:15 -0600

    Finalized lollipop plot function

--- Lollipop.py
+++ Lollipop.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def lollipop_ts(earthquake):
+def lollipop_ts(earthquakes):
+	#extract time and magnitude from class
+	new_time = [q.get_time() q in earthquakes]
+	new_mag = [q.get_mag() for q in earthquakes]
+	
 	plt.plot(new_time, new_mag, marker = 'o')
 	plt.xlabel("Date")
 	plt.ylabel("Earthquake Magnitude")
 	plt.grid(True)
  	plt.title("Earthquake Magnitude Time Series Plot")
+	plt.show()
 	
