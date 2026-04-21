e540d2ed069979cadd68193ac87c1c2a218545d4
BigMT67 <morganturner1216@gmail.com>
2026-04-21 00:05:41 -0600

    Potential fix for data format errors

--- filter.py
+++ filter.py
@@ -1,16 +1,20 @@
 import pandas as pd
+
+#I'm not sure how well the formatting matches up with how the read_cat function works,
+#because that function returns individual arrays rather than a pd object, and a pd object is what my function requires.
+
 def filter_bounds(eq,minlat,maxlat,minlong,maxlong):
 	#Use a boolean index filtering line to transform the pd dataframe object into a filtered one within the bounds.
 	#I thought I was going to have to do some crazy for loops to do this, but after some research apparently this is possible.
     bounded_eqs = eq[
-    	(eq['latitude'] >= minlat) & 
-        (eq['latitude'] <= maxlat) & 
-        (eq['longitude'] >= minlon) & 
-        (eq['longitude'] <= maxlon)]
+    	(eq['lat'] >= minlat) & 
+        (eq['lat'] <= maxlat) & 
+        (eq['long'] >= minlon) & 
+        (eq['long'] <= maxlon)]
     return bounded_eqs
 
 def filter_time(eq,starttime,endtime)
-	#Apply another boolean index filtering line. Hopefully my formatting is right for these.
+	#Apply another boolean index filtering line.
 	timed_eqs = eq[
         (eq['time'] >= start_time) & 
         (eq['time'] <= end_time)
